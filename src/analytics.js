465830829bf68f13ab2c215239c4427740c964ec
Yextds <yext@dotsquares.com>
2022-08-30 16:54:11 +0530

    update analytics codes

--- src/analytics.js
+++ src/analytics.js
@@ -1,39 +1,58 @@
 function seed() {
-    return Date.now() + Math.floor(1000 * Math.random());
-  }
-  
-  function pixelURL(data) {
-    data.v = seed();
-    let p = '//www.yext-pixel.com/store_pagespixel?product=sites';
-    for (var key in data) {
-      if (data.hasOwnProperty(key) && !!data[key]) {
-        p += `&${key}=${encodeURIComponent(data[key])}`;
-      }
+  return Date.now() + Math.floor(1000 * Math.random());
+}
+
+function pixelURL(data) {
+  data.v = seed();
+  let p = '//realtimeanalytics.yext.com/store_pagespixel?product=sites';
+  for (var key in data) {
+    if (data.hasOwnProperty(key) && !!data[key]) {
+      p += `&${key}=${encodeURIComponent(data[key])}`;
     }
-  
-    return p;
   }
-  
-  window.trackAnalytics = (businessId, siteId, entityId) => {
-    if (isStaging()) {
-      return;
+
+  return p;
+}
+
+function getCookie(cookie_name) {
+  let name = cookie_name + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
     }
-  
-    var jsonData = {
-      'pagesReferrer': window.document.referrer,
-      'pageurl': window.location.pathname,
-      'eventType': "pageview",
-      'businessids': businessId,
-      'siteId': siteId,
-      'ids': entityId,
-    };
-      
-    var pixel = pixelURL(jsonData);
-    const px = document.createElement("img");
-    px.src = pixel;
-    px.style.width = '0';
-    px.style.height = '0';
-    px.style.position = 'absolute';
-    px.alt = '';
-    document.body.appendChild(px);
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+window.trackAnalytics = (businessId, siteId, entityId) => {
+  // if (isStaging()) {
+  //   return;
+  // }
+
+  var jsonData = {
+    'pagesReferrer': window.document.referrer,
+    'pageurl': window.location.pathname,
+    'eventType': "pageview",
+    'businessids': businessId,
+    'siteId': siteId,
+    'ids': entityId,
+    '_yfpc': getCookie("_yfpc"),
   };
+    
+  var pixel = pixelURL(jsonData);
+  const px = document.createElement("img");
+  px.src = pixel;
+  px.style.width = '0';
+  px.style.height = '0';
+  px.style.position = 'absolute';
+  px.alt = '';
+  document.body.appendChild(px);
+  // console.log("trackAnalytics fired");
+  // console.log(jsonData);
+};
